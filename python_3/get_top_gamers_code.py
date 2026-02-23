41002ecf0b80dc4f9a92c2166dc4281a9ecc7443
coursecoder <coleen@coursecoder.com>
2026-02-23 01:27:44 -0500

    Redesign website UI - dark gaming aesthetic

--- python_3/get_top_gamers_code.py
+++ python_3/get_top_gamers_code.py
@@ -5,27 +5,21 @@ TABLE_NAME_STR = 'LeaderBoard'
 INDEX_NAME_STR = 'special_GSI'
 DDB = boto3.resource('dynamodb', region_name='us-east-1')
 TABLE = DDB.Table(TABLE_NAME_STR)
-    
+
 def lambda_handler(event, context):
-    # return top gamers with score greater than 3000
-    print("Running scan on index...")
+    # return top 5 gamers sorted by score descending
+    print("Running query on index...")
     response = TABLE.query(
         IndexName=INDEX_NAME_STR,
-        ScanIndexForward=False,
-        KeyConditionExpression=Key('special').eq(1) & Key('score').gt(3000),
+        ScanIndexForward=False,  # descending order (highest score first)
+        KeyConditionExpression=Key('special').eq(1),
+        Limit=5
     )
-        
-    data = response['Items']
-    
 
-    
-    while 'LastEvaluatedKey' in response:
-        response = TABLE.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        print("We needed to paginate and extend the response")
-        data.extend(response['Items'])
-        
-    #python returns non standard JSON
-    #so we need to convert some data to integers
+    data = response['Items']
+
+    # python returns non standard JSON
+    # so we need to convert some data to integers
     for item in data:
         item['score_int'] = item.pop('score')
         if item.get('special') is not None:
@@ -34,7 +28,7 @@ def lambda_handler(event, context):
         item['rank_int'] = item.pop('rank')
         item['gamer_name_str'] = item.pop('gamer_name')
         item['gamer_id_str'] = item.pop('gamer_id')
-       
+
         if item['score_int']:
             item['score_int'] = int(item['score_int'])
         if item['rank_int']:
@@ -42,7 +36,6 @@ def lambda_handler(event, context):
         if item.get('special_int') is not None:
             item['special_int'] = int(item['special_int'])
 
-    return_me={"leaderboard_item_arr": data}
-    
+    return_me = {"leaderboard_item_arr": data}
+
     return return_me
-    
